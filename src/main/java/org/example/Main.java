c96e25defef116874614ba747a91d1b441a7491f
nakayoshi-dango <159042010+nakayoshi-dango@users.noreply.github.com>
2024-03-05 17:21:37 +0100

    Commit de Fran
    Creado el bucle, añadida la descripción y añadido el método para mostrar las personas con familia numerosa.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        String a;
         ArrayList<Person> people = new ArrayList<>();
         Person p1 = new Person("Paco", 02, 11, 2004);
         people.add(p1);
@@ -17,13 +19,49 @@ public class Main {
         people.add(p4);
         Person p5 = new Person("Joseph", 02, 11, 2004);
         people.add(p5);
+        Person p6 = new Person("Manolo", 12, 04, 1984);
+        people.add(p6);
         
-        listPersons(people);
+        p1.addBrother("Francisco");
+        p1.addBrother("Fernando");
+        p1.addBrother("Luis");
+        p1.setFirstParent("Paco");
+        p1.setSecondParent("Lucía");
+        
+        System.out.println("-----------Descripción de la aplicación------------");
+        System.out.println("En esta clase Main, se prueban los métodos que");
+        System.out.println("consultan datos sobre los distintos objetos creados.");
+        System.out.println("Las personas, clases y asignaturas vendrán definidas");
+        System.out.println("y no se podrán crear nuevas durante la ejecución.");
+        System.out.println("----------------------------------------------------");
+        do {
+            System.out.println("Introduzca un número para: ");
+            System.out.println("1) Listar a todas las personas");
+            System.out.println("2) Listar a las personas que tengan familia numerosa.");
+            
+            a = s.nextLine();
+            if (!a.equals("0")) {
+                if (a.equals("1")) {
+                    listPersons(people);
+                } else if (a.equals("2")) {
+                    familiasNumerosas(people);
+                }
+            }
+        } while (!a.equals("0"));
     }
-    
-    public static void listPersons(ArrayList<Person> people){
+
+    public static void listPersons(ArrayList<Person> people) {
+        System.out.println("Listado de personas:");
         for (Person personTmp : people) {
             System.out.println(personTmp);
+        }
+    }
+
+    public static void familiasNumerosas(ArrayList<Person> people) {
+        for (Person person : people) {
+            if (person.largeFamily()) {
+                System.out.println(person);
+            }
         }
     }
 }
