214fac06fbda2fca616f5707176d2b4338e20bf0
Winchal <oscar.roldan@educa.madrid.org>
2024-03-05 18:48:58 +0100

    Oscar

--- src/main/java/org/example/Clase1.java
+++ src/main/java/org/example/Clase1.java
@@ -1,33 +1,29 @@
-package org.example;
+package codeclean;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Clase1 {
-    public String nombre_Clase;
-    public int nPLAZAS = 5;
-    clase2[] asignaturra = new clase2[nPLAZAS];
+    public String nombreClase;
+    public int numeroMaxAsignatura = 5;
+    //numeroPlazas se tiene que llamar numeroMaxAsignatura
+    clase2[] asignatura = new clase2[numeroPlazas]; // DEBERIA DE SER UN ARRAYLIST DE CLASE2
 
-    Clase1(String n) {
-        nombre_Clase = n;
+    Clase1(String n) { //¿DE DONDE VIENE N?
+        nombreClase = n;
     }
+    //HABRIA QUE HACER UN ARRAYLIST DE ASIGNATURA
+    //AS COMO MAXIMO PUEDE TENER 5 ASIGNATURAS
+    // añade asignatura si pasan cosas...
 
-    // aniade asignatura si pasan cosas...
-    public void aniadirAsignaturra(clase2 as) throws Exception {
-        if(asignaturra[0] != null)
-            if(asignaturra[1] != null)
-                if(asignaturra[2] != null)
-                    if(asignaturra[3] != null)
-                        if(asignaturra[4]!=null)
-                            throw new Exception("a");
-        else
-            asignaturra[4] = as;
-        else
-            asignaturra[3] = as;
-        else
-            asignaturra[2] = as;
-        else asignaturra[1] = as;
-        else asignaturra[0] = as;
 
+    //NO ES ASIGNATURA ES CONTAR ASIGNATURA 
+    public void contarAsignatura(clase3 persona) throws Exception { // NO TIENE SENTIDO PASAR  CLASE2 AS HABRIA QUE PASAR ALGO RELLENADO
+        for (int i = 0; i < asignatura.length; i++) { //NO TIENE SENTIDO O NO LO ENTIENDO YO 
+            if(asignatura[i] == null) asignatura[i] = persona;
+            else if(i == 4 && asignatura[i] != null){
+                throw new Exception("a");
+            }
+        }
     }
 }
