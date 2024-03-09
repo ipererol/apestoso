298df1ed58143672d89296a6c75fff2650965a83
dev <darius.benta@educa.madrid.org>
2024-03-09 14:26:07 +0100

    He cambiado el nombre de las variables en ingles

--- src/main/java/org/example/Clase_3.java
+++ src/main/java/org/example/Clase_3.java
@@ -3,44 +3,44 @@ package org.example;
 import java.util.ArrayList;
 
 public class Clase_3 {
-    public float nombreAsignatura ;
-    public int maxNAlumnos = 50;
-    public int intAlumnosMatriculados = 0;
-    ArrayList<clase2> alumnado ;
+    public float subjectName ;
+    public int maxNStudents = 50;
+    public int matriculatedStudents = 0;
+    ArrayList<clase2> students ;
 
-    public Clase_3(float nombre) {
-        nombreAsignatura = nombre;
-        alumnado =   new ArrayList<>();
+    public Clase_3(float name) {
+        subjectName = name;
+        students =   new ArrayList<>();
     }
 
     public int añadirPersona(clase2 classeo) {
-        if (intAlumnosMatriculados < 50) {
-            alumnado.set(intAlumnosMatriculados, classeo);
-            intAlumnosMatriculados++;
+        if (matriculatedStudents < 50) {
+            students.set(matriculatedStudents, classeo);
+            matriculatedStudents++;
             return 1;
         } else {
             return 0;
         }
     }
 
-    public clase2 nombrarAlumno(int n) throws Exception {
-        if (alumnado[n] == null)
-            return alumnado[n];
+    public clase2 nameStudent(int n) throws Exception {
+        if (students[n] == null)
+            return students[n];
         else
             throw new Exception("excepcion");
     }
 
-    public boolean darDeBajaAlumno(String nombre) {
+    public boolean unsuscribeStudent(String name) {
         int i = 0;
-        for(clase2 c4 : alumnado) {
-            if(alumnado[i].equals(new clase2(nombre)))
+        for(clase2 c4 : students) {
+            if(students[i].equals(new clase2(name)))
                 break;
             i++;
         }
         if(i==50) {
             return false;
         } else {
-            alumnado[i] = null;
+            students[i] = null;
             return false;
         }
     }
