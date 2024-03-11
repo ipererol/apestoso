88bf2a2fc62156f8586651c9ce99015a252b1a4f
javiercotillas <114167746+javiercotillas@users.noreply.github.com>
2024-03-11 22:01:27 +0100

    Update and rename
    Renombramos clase2 por Person.
    Renombramos el nombre de nuestra clase a Degree.

--- src/main/java/org/example/Degree.java
+++ src/main/java/org/example/Degree.java
@@ -3,18 +3,18 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Clase1 {
+public class Degree {
 
     public String nameDegree;
-    ArrayList<clase2> listSubject = new ArrayList<>();
+    ArrayList<Person> listSubject = new ArrayList<>();
 
-    Clase1(String nameDegree, ArrayList<clase2> listSubject) {
+    Degree(String nameDegree, ArrayList<Person> listSubject) {
         this.nameDegree = nameDegree;
         this.listSubject = listSubject;
     }
 
     // aniade asignatura si pasan cosas...
-    public void aniadirAsignaturra(clase2 as) throws Exception {
+    public void aniadirAsignaturra(Person as) throws Exception {
         if (listSubject.size() == 5) {
 //aniadir excepcion si la lista de asignaturas es mayor que 5
         } else {
