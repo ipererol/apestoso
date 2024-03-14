b702d2e79523a5709d989bc85457d36e34f0ab8c
Nico <neekoxx4@gmail.com>
2024-03-14 23:52:50 +0100

    if exception modificada clase 1

--- src/main/java/org/example/Clase1.java
+++ src/main/java/org/example/Clase1.java
@@ -2,12 +2,14 @@ package codeclean;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.example.clase2;
 
 public class Clase1 {
-    public String nombreClase; 
+
+    public String nombreClase;
     public int numeroMaxAsignatura = 5;
     //numeroPlazas se tiene que llamar numeroMaxAsignatura
-    clase2[] asignatura = new clase2[numeroPlazas]; // DEBERIA DE SER UN ARRAYLIST DE CLASE2
+    clase2[] asignatura = new clase2[numeroMaxAsignatura]; // DEBERIA DE SER UN ARRAYLIST DE CLASE2
 
     Clase1(String n) { //¿DE DONDE VIENE N?
         nombreClase = n;
@@ -16,13 +18,14 @@ public class Clase1 {
     //AS COMO MAXIMO PUEDE TENER 5 ASIGNATURAS
     // añade asignatura si pasan cosas...
 
-
     //NO ES ASIGNATURA ES CONTAR ASIGNATURA 
-    public void contarAsignatura(clase3 persona) throws Exception { // NO TIENE SENTIDO PASAR  CLASE2 AS HABRIA QUE PASAR ALGO RELLENADO
+    public void contarAsignatura(clase2 persona) throws Exception { // NO TIENE SENTIDO PASAR  CLASE2 AS HABRIA QUE PASAR ALGO RELLENADO
         for (int i = 0; i < asignatura.length; i++) { //NO TIENE SENTIDO O NO LO ENTIENDO YO 
-            if(asignatura[i] == null) asignatura[i] = persona;
-            else if(i == 4 && asignatura[i] != null){
-                throw new Exception("a");
+            if (asignatura[i] == null) {
+                asignatura[i] = persona;
+            }
+            if (i > numeroMaxAsignatura && asignatura[i] != null) {
+                throw new Exception("No puede haber mas de cinco asignaturas");
             }
         }
     }
