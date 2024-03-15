5e220bf543dd0dd583b1a1a7337559d55cbaafd1
Villaescusahector <110830365+Villaescusahector@users.noreply.github.com>
2024-03-15 19:44:58 +0100

    incorporacion getter and setters

--- src/main/java/org/example/Clase1.java
+++ src/main/java/org/example/Clase1.java
@@ -2,38 +2,37 @@ package codeclean;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.example.clase2;
 
-public class Clase1 {
-    public String nombreClase;
-    public int numeroPlazas = 5;
-    clase2[] asignatura = new clase2[numeroPlazas];
-
-    Clase1(String n) {
-        nombreClase = n;
+public class Subject {
+    private float subjectName;
+    private ArrayList<clase2> people;
+    private int MAX_ALUMNOS;
+    
+    public Subject(float SubjectName, ArrayList<clase2> people, int MAX_ALUMNOS){
+        this.subjectName = SubjectName;
+        this.people = people;
+        this.MAX_ALUMNOS = MAX_ALUMNOS;
+    }
+    
+    public float getSubjectName(){
+        return subjectName;
     }
 
-    // añade asignatura si pasan cosas...
-    public void aniadirAsignaturra(clase2 as) throws Exception {
-        if (asignatura[0] != null) {
-            if (asignatura[1] != null) {
-                if (asignatura[2] != null) {
-                    if (asignatura[3] != null) {
-                        if (asignatura[4] != null) {
-                              throw new Exception("a");
-                        } else {
-                            asignatura[4] = as;
-                        }
-                    } else {
-                        asignatura[3] = as;
-                    }
-                } else {
-                    asignatura[2] = as;
-                }
-            } else {
-                asignatura[1] = as;
-            }
-        } else {
-            asignatura[0] = as;
-        }
+    public void setSubjectName(float subjectName){
+        this.subjectName = subjectName;
     }
+
+    public ArrayList<clase2> getPeople(){
+        return people;
+    }
+
+    public void setPeople(ArrayList<cñase2> people){
+        this.people = people;
+    }
+
+    public int getMAX_ALUMNOS(){
+        return MAX_ALUMNOS;  
+    }
+
 }
