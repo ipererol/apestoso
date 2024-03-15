59c0dc7a88cbbc307c8366e6a12f2d9765187ed3
danielantmann <tula3x3@gmail.com>
2024-03-15 22:29:33 +0100

    Arreglo Classroom.java

--- src/main/java/org/example/Classroom.java
+++ src/main/java/org/example/Classroom.java
@@ -4,35 +4,35 @@ import java.util.*;
 
 public class Classroom {
 
-    public String nombreAsignatura;
-    public ArrayList<clase2> alumnado;
-    public static final int MAX_ALUMNO = 50;
-    public static int ALUMNOS_MATRICULADOS = 0;
+    public String degreeName;
+    public ArrayList<Student> students;
+    public static final int MAX_STUDENTS = 50;
+    public static int studentsEnrolled = 0;
 
-    public Classroom(String nombreAsignatura, ArrayList<clase2> alumnado) {
-        this.nombreAsignatura = nombreAsignatura;
-        this.alumnado = alumnado;
+    public Classroom(String nombreAsignatura, ArrayList<Student> alumnado) {
+        this.degreeName = nombreAsignatura;
+        this.students = alumnado;
     }
 
-    public void anyadirPersona(clase2 classeo) throws Exception {
-        if (ALUMNOS_MATRICULADOS < 50) {
-            alumnado.add(classeo);
-            ALUMNOS_MATRICULADOS++;
+    public void addPersonToClasroom(clase2 classeo) throws Exception {
+        if (studentsEnrolled < 50) {
+            students.add(classeo);
+            studentsEnrolled++;
         }
     }
 
     public clase2 nombrarAlumno(int n) throws Exception {
-        if (alumnado.get(n) != null) {
-            return alumnado.get(n);
+        if (students.get(n) != null) {
+            return students.get(n);
         } else {
             throw new Exception("Error, no existe el alumno");
         }
     }
 
     public void darDeBajaAlumno(String nombre) {
-        for (int i = 0; i < alumnado.size(); i++) {
-            if (alumnado.get(i).getNombre_ALUMNO().equals(nombre)) {
-                alumnado.remove(i);
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getNombre_ALUMNO().equals(nombre)) {
+                students.remove(i);
                 i--;
             }
         }
