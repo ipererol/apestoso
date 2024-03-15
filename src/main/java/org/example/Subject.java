e182bd2c73c4a2bb44a7ad3407e2b3657a7aab13
Daylto <dayltobravo999@gmail.com>
2024-03-15 20:23:33 +0100

    anadidos final, subject and exception

--- src/main/java/org/example/Subject.java
+++ src/main/java/org/example/Subject.java
@@ -7,7 +7,7 @@ import org.example.clase2;
 public class Subject {
     private float subjectName;
     private ArrayList<clase2> people;
-    private int MAX_ALUMNOS;
+    private final int MAX_ALUMNOS;
     
     public Subject(float SubjectName, ArrayList<clase2> people, int MAX_ALUMNOS){
         this.subjectName = SubjectName;
@@ -27,13 +27,30 @@ public class Subject {
         return people;
     }
 
-    public void setPeople(ArrayList<cñase2> people){
+    public void setPeople(ArrayList<clase2> people){
         this.people = people;
     }
 
     public int getMAX_ALUMNOS(){
         return MAX_ALUMNOS;  
     }
+    public void addStudent(clase2 Student)  throws Exception {
+       
+        if (people.size() < MAX_ALUMNOS) {
+            people.add(Student);
+
+        } else {
+            throw new Exception("Ha superado el maximo de alumnos");  
+            
+        }
+    }
+
+    public void listStudents() {
+        for (clase2 student : people) {
+            System.out.println(student);
+        }
+    }
+
 
 }
 
